cf7ceaa0e8192d5e2332657f22f71d5ab651e0a0
Adran Farias Carnavale <adran.carnavale@gmail.com>
2022-06-21 18:09:34 -0300

    feat: :fire: Install project dependendencies!
    Install dependency packages for frontend and backend modules, to start project development
    
    Installation of package dependendencies

---
+++ backend/src/server.ts
@@ -0,0 +1,6 @@
+import { app } from './app';
+import 'dotenv/config';
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export function App() {
+  return (
+    <div>
+      <h1 className="bg-zinc-500">App</h1>
+    </div>
+  );
+}
