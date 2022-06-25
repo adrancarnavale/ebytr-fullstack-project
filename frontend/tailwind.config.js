cc11cf566cc706dc4e231b25d706c5ef9138de69
Adran Farias Carnavale <adran.carnavale@gmail.com>
2022-06-25 11:51:43 -0300

    feat: :art: adds new colors to nord palette, on tailwindconfig.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,17 @@ module.exports = {
       'nord-light-1': '#D8DEE9',
       'nord-light-2': '#E5E9F0',
       'nord-light-3': '#ECEFF4',
+
+      'nord-frost-1': '#8FBCBB',
+      'nord-frost-2': '#88C0D0',
+      'nord-frost-3': '#81A1C1',
+      'nord-frost-4': '#5E81AC',
+
+      'nord-aurora-1': '#BF616A',
+      'nord-aurora-2': '#D08770',
+      'nord-aurora-3': '#EBCB8B',
+      'nord-aurora-4': '#A3BE8C',
+      'nord-aurora-5': '#B48EAD',
     },
     fontFamily: {
       sans: ['Rubik', 'Roboto', 'sans-serif'],
