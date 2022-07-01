06651c88eb15b45bf8e0b1ff797e6fb5b148486d
Adran Farias Carnavale <adran.carnavale@gmail.com>
2022-07-01 09:34:25 -0300

    feat: :memo: removes edit user route

--- backend/src/modules/User/Edit/EditIntegration.ts
+++
@@ -1,8 +0,0 @@
-import { EditImplementation } from './EditImplementation';
-import { EditUseCase } from './EditUseCase';
-import { EditController } from './EditController';
-
-const imp = new EditImplementation();
-const useCase = new EditUseCase(imp);
-
-export const edit = new EditController(useCase);
--- backend/src/modules/User/Edit/EditRepository.ts
+++
@@ -1,6 +0,0 @@
-import { Target } from '../../../@types';
-import { IUser } from '../../../entities/IUser';
-
-export interface EditRepository {
-  edit(userInfo: IUser, target: Target): Promise<IUser>;
-}
