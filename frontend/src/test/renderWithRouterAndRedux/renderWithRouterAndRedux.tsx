3b723aa9a53fb0c9a4fd3d845ac4d6be5388696b
Adran Farias Carnavale <adran.carnavale@gmail.com>
2022-07-05 15:53:01 -0300

    feat: :zap: creates generic component for frontend tests

---
+++ frontend/src/test/renderWithRouterAndRedux/renderWithRouterAndRedux.tsx
@@ -0,0 +1,34 @@
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { Store } from 'redux';
+import { MemoryHistory, createMemoryHistory } from 'history';
+import { rootReducer } from '@app';
+import { configureStore } from '@reduxjs/toolkit';
+import { unstable_HistoryRouter as Router } from 'react-router-dom';
+
+interface renderWithRouterAndReduxProps {
+  initialState: object;
+  store: Store;
+  route: string;
+  history: MemoryHistory;
+}
+
+export const renderWithRouterAndRedux = (
+  component: React.ReactElement,
+  {
+    initialState = {},
+    store = configureStore({
+      reducer: rootReducer,
+      preloadedState: initialState,
+      devTools: true,
+    }),
+    route = '/',
+    history = createMemoryHistory({ initialEntries: [route] }),
+  } = {} as renderWithRouterAndReduxProps
+) => ({
+  ...render(
+    <Provider store={store}>
+      <Router history={history}>{component}</Router>
+    </Provider>
+  ),
+});
